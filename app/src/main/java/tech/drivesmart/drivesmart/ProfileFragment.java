a71529f54aa93c89cde196c89f0e3f4bb6433a9d
Linda Yang <lindayang0928@hotmail.com>
2018-06-10 22:53:18 -0400

    edited setting interface

--- app/src/main/java/tech/drivesmart/drivesmart/ProfileFragment.java
+++ app/src/main/java/tech/drivesmart/drivesmart/ProfileFragment.java
@@ -38,7 +38,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_settings,container,false);
         initViews(view);
         return view;
     }
