146c16f323d9d4c3cf7cf4bea378cf904a9e6b52
David Zhang <37002542+davidhqr@users.noreply.github.com>
2018-07-22 23:21:52 -0400

    Various changes (#25)
    * Migrate to strings resources (#20)
    
    * Improved UI (#21)
    
    * Work on profile view (#22)
    
    * changed icon sizes (#24)
    
    * Make stop driving and logout buttons work

--- app/src/main/java/com/distractic/LoginFragment.java
+++ app/src/main/java/com/distractic/LoginFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
@@ -46,9 +47,9 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
     private void initViews(View view) {
 
-        pref = loginRegisterActivity.getPreferences(0);
+        pref = loginRegisterActivity.getSharedPreferences("info", 0);
 
-        button_loginbutton = view.findViewById(R.id.login_button_loginbutton);
+        button_loginbutton = view.findViewById(R.id.login_button_loginButton);
         edit_email = view.findViewById(R.id.login_edit_email);
         edit_password = view.findViewById(R.id.login_edit_password);
         text_register = view.findViewById(R.id.login_text_register);
@@ -67,7 +68,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
                 goToRegister();
                 break;
 
-            case R.id.login_button_loginbutton:
+            case R.id.login_button_loginButton:
                 String email = edit_email.getText().toString();
                 String password = edit_password.getText().toString();
 
@@ -109,12 +110,12 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
                 if (resp.getResult().equals(Constants.SUCCESS)) {
 
-                    SharedPreferences.Editor editor = pref.edit();
+                    Editor editor = pref.edit();
                     editor.putBoolean(Constants.IS_LOGGED_IN, true);
                     editor.putString(Constants.EMAIL, resp.getUser().getEmail());
                     editor.putString(Constants.FIRST_NAME, resp.getUser().getFirstName());
                     editor.putString(Constants.LAST_NAME, resp.getUser().getLastName());
-                    editor.putString(Constants.UNIQUE_ID, resp.getUser().getUnique_id());
+                    editor.putString(Constants.UNIQUE_ID, resp.getUser().getUniqueId());
                     editor.apply();
                     goToProfile();
                 }
--- app/src/main/java/com/distractic/ProfileActivity.java
+++ app/src/main/java/com/distractic/ProfileActivity.java
@@ -1,13 +1,96 @@
 package com.distractic;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.distractic.util.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    private SharedPreferences pref;
+    private Button button_startDriving, button_logout;
+    private TextView text_name, text_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        pref = getSharedPreferences("info", 0);
+
+        button_startDriving = findViewById(R.id.profile_button_startDriving);
+        button_logout = findViewById(R.id.profile_button_logout);
+        text_name = findViewById(R.id.profile_text_name);
+        text_location = findViewById(R.id.profile_text_location);
+
+        button_startDriving.setOnClickListener(this);
+        button_logout.setOnClickListener(this);
+
+        setText();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int hasCameraPermission = checkSelfPermission(Manifest.permission.CAMERA);
+            List<String> permissions = new ArrayList<String>();
+
+            if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.CAMERA);
+            }
+            if (!permissions.isEmpty()) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), 111);
+            }
+        }
+    }
+
+    private void setText() {
+        Map<String,?> keys = pref.getAll();
+
+        text_name.setText(pref.getString(Constants.FIRST_NAME, "") + " " + pref.getString(Constants.LAST_NAME, ""));
+        text_location.setText("Vancouver, BC");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 111:
+                for (int i = 0; i < permissions.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("Permissions --> " + "Permission Granted: " + permissions[i]);
+                    } else if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        System.out.println("Permissions --> " + "Permission Denied: " + permissions[i]);
+                    }
+                }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.profile_button_startDriving:
+                Intent cameraIntent = new Intent(ProfileActivity.this, CameraActivity.class);
+                startActivity(cameraIntent);
+                break;
+            case R.id.profile_button_logout:
+                Editor editor = pref.edit();
+                editor.clear();
+                editor.apply();
+                Intent loginRegisterIntent = new Intent(ProfileActivity.this, LoginRegisterActivity.class);
+                startActivity(loginRegisterIntent);
+                break;
+        }
     }
 }
